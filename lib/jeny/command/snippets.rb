5e745b4231b53a93351a92dc00610ae9bc88bc10
Bernard Lambeau <blambeau@gmail.com>
2020-12-30 10:35:17 +0100

    Fail with intelligible error when snippet fails on a file.

--- lib/jeny/command/snippets.rb
+++ lib/jeny/command/snippets.rb
@@ -18,35 +18,40 @@ module Jeny
         changed = from.glob("**/*").map{|source|
           next if source.directory?
           next if config.ignore_file?(source)
-
-          target, target_content = nil
-          if source.ext =~ /\.?jeny/
-            file = File::Full.new(source, config)
-            if file.has_jeny_context?
-              ctx = file.instantiate_context(data)
-              target_content = file.instantiate(ctx)
-              target = target_for(source, ctx)
-              target.parent.mkdir_p
-              target.write(target_content)
-              puts "snippets #{simplify_path(target)}"
-            end
-          else
-            file = File::WithBlocks.new(source, config)
-            if file.has_jeny_blocks?
-              target_content = file.instantiate(data)
-              target = target_for(source)
-              target.write(target_content)
-              puts "snippets #{simplify_path(target)}"
-            end
-          end
-
-          target ? [target, target_content] : nil
+          snippet_it(source)
         }.compact
 
         to_open = changed
           .select{|pair| config.should_be_edited?(*pair) }
           .map{|pair| simplify_path(pair.first) }
         config.open_editor(to_open) unless to_open.empty?
+      end
+
+      def snippet_it(source)
+        target, target_content = nil
+        if source.ext =~ /\.?jeny/
+          file = File::Full.new(source, config)
+          if file.has_jeny_context?
+            ctx = file.instantiate_context(data)
+            target_content = file.instantiate(ctx)
+            target = target_for(source, ctx)
+            target.parent.mkdir_p
+            target.write(target_content)
+            puts "snippets #{simplify_path(target)}"
+          end
+        else
+          file = File::WithBlocks.new(source, config)
+          if file.has_jeny_blocks?
+            target_content = file.instantiate(data)
+            target = target_for(source)
+            target.write(target_content)
+            puts "snippets #{simplify_path(target)}"
+          end
+        end
+        target ? [target, target_content] : nil
+      rescue => ex
+        msg = "Error in `#{simplify_path(source)}`: #{ex.message}"
+        raise Error, msg
       end
 
     end # class Snippets
